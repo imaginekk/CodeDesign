a06104222e0395d4db81da9a159be93beb0ecddd
Imagine <zyh10_27@petalmail.com>
2023-07-11 16:39:32 +0800

    imagine,feat:修改目录结构

---
+++ Factory/src/main/java/factory/award/AwardRes.java
@@ -0,0 +1,33 @@
+package factory.award;
+
+/**
+ * @description 发奖结果反馈对象
+ * @date 2022/3/12
+ */
+public class AwardRes {
+
+    private String code; // 编码
+    private String info; // 描述
+
+    public AwardRes(String code, String info) {
+        this.code = code;
+        this.info = info;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+}
---
+++ Factory/src/main/java/factory/card/IQiYiCard.java
@@ -0,0 +1,11 @@
+package factory.card;
+
+/**
+ * @description 模拟爱奇艺视频卡，对象类
+ * @date 2022/3/12
+ */
+public class IQiYiCard {
+
+     // 卡券的一些信息
+
+}
